d6b32343e4f092c050526f429559ef61191abfa7
truong <truonghb29@gmail.com>
2023-03-04 23:05:29 +0700

    bai1 lab4

---
+++ KeThua/src/HinhHoc.java
@@ -0,0 +1,66 @@
+
+
+public class HinhHoc {
+    private float PI = 3.14f;
+    private String ten;
+    private float chuVi;
+    private float dienTich;
+    private float theTich;
+
+    public HinhHoc() {
+    }
+
+    public float getPI() {
+        return PI;
+    }
+
+    public void setPI(float PI) {
+        this.PI = PI;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public float getChuVi() {
+        return chuVi;
+    }
+
+    public void setChuVi(float chuVi) {
+        this.chuVi = chuVi;
+    }
+
+    public float getDienTich() {
+        return dienTich;
+    }
+
+    public void setDienTich(float dienTich) {
+        this.dienTich = dienTich;
+    }
+
+    public float getTheTich() {
+        return theTich;
+    }
+
+    public void setTheTich(float theTich) {
+        this.theTich = theTich;
+    }
+
+    public void xuatTen() {
+    }
+
+    public void inChuVi() {
+    }
+
+    public void inDienTich() {
+    }
+
+    public void inTheTich() {
+    }
+}
+
+
